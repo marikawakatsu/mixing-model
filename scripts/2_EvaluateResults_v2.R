3854050396d95b5f305263a09c0477c3de881eac
marikawakatsu <20093432+marikawakatsu@users.noreply.github.com>
2018-11-15 17:07:11 -0500

    More alpha simulations

--- scripts/2_EvaluateResults_v2.R
+++ scripts/2_EvaluateResults_v2.R
@@ -20,7 +20,7 @@ reps           <- 10 #number of replications per simulation (for ensemble) !!Cha
 
 # Threshold Parameters
 mixes          <- c("A", "B", "AB")
-A_ThreshM      <- c(20, 20) #population threshold means for clone line A !!Change!!
+A_ThreshM      <- c(10, 10) #population threshold means for clone line A !!Change!!
 A_ThreshSD     <- A_ThreshM * 0.1 #population threshold standard deviations for clone line A !!Change!!
 B_ThreshM      <- c(10, 10) #population threshold means for clone line B !!Change!!
 B_ThreshSD     <- B_ThreshM * 0.1 #population threshold standard deviations for clone line B !!Change!!
@@ -28,8 +28,8 @@ InitialStim    <- c(0, 0) #intital vector of stimuli
 deltas         <- c(0.6, 0.6) #vector of stimuli increase rates  
 threshSlope    <- 7 #exponent parameter for threshold curve shape
 alpha          <- m
-A_alpha        <- c(m*3, m*3) #efficiency of task performance
-B_alpha        <- c(m, m)
+A_alpha        <- c(m, m*3) #efficiency of task performance
+B_alpha        <- c(m, m*3)
 quitP          <- c(0.2, 0.2) #probability of quitting task once active !!Change!!
 
 file_name <- sprintf("AThreshM_%1.2f_%1.2f_AThreshSD_%1.2f_%1.2f_BThreshM_%1.2f_%1.2f_BThreshSD_%1.2f_%1.2f_deltas_%1.2f_%1.2f_threshSlope_%d_%d_Aalpha_%1.2f_%1.2f_Balpha_%1.2f_%1.2f_quitP_%1.2f_%1.2f",
