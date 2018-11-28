34e37ba4679868047997f4ff3fadcbd634278c88
marikawakatsu <20093432+marikawakatsu@users.noreply.github.com>
2018-11-27 20:16:05 -0500

    Draft v2 updated

--- scripts/2_EvaluateResults_v3.R
+++ scripts/2_EvaluateResults_v3.R
@@ -3,8 +3,8 @@
 # Evaluate ensemble model outputs
 #
 ################################################################################
-
 rm(list = ls())
+
 source("scripts/util/__Util__MASTER.R")
 
 ####################
@@ -28,13 +28,13 @@ InitialStim    <- c(0, 0) #intital vector of stimuli
 deltas         <- c(0.6, 0.6) #vector of stimuli increase rates  
 threshSlope    <- 7 #exponent parameter for threshold curve shape
 alpha          <- m
-A_alpha        <- c(m, m) #efficiency of task performance
-B_alpha        <- c(m*3, m*3)
+A_alpha        <- c(m, m*3) #efficiency of task performance
+B_alpha        <- c(m, m*3)
 quitP          <- c(0.2, 0.2) #probability of quitting task once active !!Change!!
 
 file_name1 <- sprintf("New_AThreshM_%1.2f_%1.2f_BThreshM_%1.2f_%1.2f_deltas_%1.2f_%1.2f_threshSlope_%d_Aalpha_%1.2f_%1.2f_Balpha_%1.2f_%1.2f_quitP_%1.2f",
                       A_ThreshM[1], A_ThreshM[2], B_ThreshM[1], B_ThreshM[2], deltas[1], deltas[2], threshSlope, 
-                      A_alpha[1], A_alpha[2], B_alpha[1], B_alpha[2], quitP[1])  # for quitp[1] = quitP[2]
+                      A_alpha[1], A_alpha[2], B_alpha[1], B_alpha[2], quitP[1])
 
 file_name2 <- sprintf("New_AThreshM_%1.2f_%1.2f_AThreshSD_%1.2f_%1.2f_BThreshM_%1.2f_%1.2f_BThreshSD_%1.2f_%1.2f_deltas_%1.2f_%1.2f_threshSlope_%d_%d_Aalpha_%1.2f_%1.2f_Balpha_%1.2f_%1.2f_quitP_%1.2f_%1.2f",
                       A_ThreshM[1], A_ThreshM[2], A_ThreshSD[1]/A_ThreshM[1], A_ThreshSD[2]/A_ThreshM[2], 
@@ -42,7 +42,7 @@ file_name2 <- sprintf("New_AThreshM_%1.2f_%1.2f_AThreshSD_%1.2f_%1.2f_BThreshM_%
                       deltas[1], deltas[2], threshSlope, threshSlope, A_alpha[1], A_alpha[2], 
                       B_alpha[1], B_alpha[2], quitP[1], quitP[2])
 
-file_name <- file_name2
+file_name <- file_name1
 rm(file_name1, file_name2)
 
 load(paste0("output/Rdata/", file_name, ".Rdata"))
