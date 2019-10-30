a30d914f058a4e68af6edac9cff3cfe818a94fda
Mari Kawakatsu <20093432+marikawakatsu@users.noreply.github.com>
2019-10-30 17:22:50 -0400

    Updating Fig 5

--- scripts/2_Robustness_Fig4.R
+++ scripts/2_Robustness_Fig4.R
@@ -79,7 +79,8 @@ for (INDEX in 1:nrow(params)){
     # load data
     ####################
     load(paste0("output/Rdata/", file_name2, "_robust_50.Rdata"))
-    
+    # readRDS(paste0("output/Rdata/", file_name2, "_robust_50.Rdata"))
+
     ####################
     # process data
     ####################
@@ -242,6 +243,6 @@ gg_amps
 
 # ggsave(filename = paste0("output/Parameter_exp/Parameter_space_sample.png"), width = 2.90, height = 2.10, units = "in",  dpi = 800)
 # ggsave(filename = paste0("output/Parameter_exp/Parameter_space_sample_v2.png"), width = 2.90, height = 2.10, units = "in",  dpi = 800)
-# ggsave(filename = paste0("output/Parameter_exp/Parameter_space_sample_50.png"), width = 2.90, height = 2.10, units = "in",  dpi = 800)
+ggsave(filename = paste0("output/Parameter_exp/Parameter_space_sample_50.png"), width = 2.90, height = 2.10, units = "in",  dpi = 800)
 
 
